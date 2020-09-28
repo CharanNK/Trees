7b9d9fe20ecb9b6ef62a81e7ea0f141772c2daa5
CharanNK <charkeet@gmail.com>
2020-09-29 01:48:07 +0530

    Basic tree

--- src/basics/Tree.java
+++ src/basics/Tree.java
@@ -1,5 +1,31 @@
 package basics;
 
-public class Tree {
+class Node{
+	int key;
+	Node root;
+	Node left;
+	Node right;
+	Node(int k){
+		key = k;
+	}
+}
 
+public class Tree {
+	
+	static void inorder(Node root) {
+		if(root!=null) {
+			inorder(root.left);
+			System.out.print(root.key+" ");
+			inorder(root.right);
+		}
+	}
+	
+	public static void main(String[] args) {
+		Node root = new Node(10);
+		root.left = new Node(20);
+		root.right = new Node(30);
+		root.left.left = new Node(40);
+		
+		inorder(root);
+	}
 }
