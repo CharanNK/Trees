8aa5029a32645085fefe9c498efc4ce90dbf4279
CharanNK <charkeet@gmail.com>
2020-09-29 02:01:33 +0530

    Height and traversals

---
+++ src/basics/TreeHeight.java
@@ -0,0 +1,27 @@
+package basics;
+
+public class TreeHeight {
+	
+	static int height(Node root) {
+		if(root==null) 
+			return 0;
+		else {
+			return Math.max(height(root.left), height(root.right))+1;
+		}
+	}
+
+	public static void main(String[] args) {
+		Node root = new Node(10);
+		root.left = new Node(20);
+		root.right = new Node(30);
+		root.left.left = new Node(40);
+		root.left.left.right = new Node(50);
+		root.right.right = new Node(60);
+		root.right.right.left = new Node(70);
+		root.right.right.right = new Node(80);
+		root.right.right.right.right = new Node(90);
+		
+		int treeHeight = height(root);
+		System.out.println("Height of tree : "+treeHeight);
+	}
+}
---
+++ src/basics/TreeTraversals.java
@@ -0,0 +1,56 @@
+package basics;
+
+//class Node{
+//	int key;
+//	Node root;
+//	Node left;
+//	Node right;
+//	Node(int k){
+//		key = k;
+//	}
+//}
+
+public class TreeTraversals {
+	
+	static void inorder(Node root) {
+		if(root!=null) {
+			inorder(root.left);
+			System.out.print(root.key+" ");
+			inorder(root.right);
+		}
+	}
+	
+	static void preorder(Node root) {
+		if(root!=null) {
+			System.out.print(root.key+" ");
+			inorder(root.left);
+			inorder(root.right);
+		}
+	}
+	
+	static void postorder(Node root) {
+		if(root!=null) {
+			postorder(root.left);
+			postorder(root.right);
+			System.out.print(root.key+" ");
+		}
+	}
+
+	public static void main(String[] args) {
+		Node root = new Node(10);
+		root.left = new Node(20);
+		root.right = new Node(30);
+		root.left.left = new Node(40);
+		root.left.left.right = new Node(50);
+		root.right.right = new Node(60);
+		root.right.right.left = new Node(70);
+		root.right.right.right = new Node(80);
+		
+		inorder(root);
+		System.out.println(System.lineSeparator());
+		preorder(root);
+		System.out.println(System.lineSeparator());
+		postorder(root);
+	}
+
+}
